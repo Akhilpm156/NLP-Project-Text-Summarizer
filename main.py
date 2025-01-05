39f59d960c621b048e95e26d280969becf0273d0
Akhilpm156 <akhilpm156@gmail.com>
2025-01-05 20:20:17 +0400

    data_ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.stage_01 import data_ingestion_training_pipeline
 from textSummarizer.logging import logger
 
-logger.info("Testing logger import from the textSummarizer package!")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = data_ingestion_training_pipeline
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
